d02af52e7a629cd5ff875dea86ad9c7205c53e4a
Chaitanya Yadav <chaitanya.yadav@thredup.com>
2019-06-24 14:12:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu } from 'antd';
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">Retail Store Shift Schedular</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px', marginTop: 64 }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
+            Hello World
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Retail Store Shift Schedular ©2019 Created by Team IIITA</Footer>
+      </Layout>
     </div>
   );
 }
